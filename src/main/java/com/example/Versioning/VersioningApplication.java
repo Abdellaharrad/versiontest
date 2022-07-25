216b2d87d53b36cc262e4864361fa6f15223214e
Abdellaharrad <arradabdellah1@gmail.com>
2022-07-25 16:15:55 +0100

    feat: add controller

--- src/main/java/com/example/Versioning/VersioningApplication.java
+++ src/main/java/com/example/Versioning/VersioningApplication.java
@@ -2,9 +2,16 @@ package com.example.Versioning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.*;
 
 @SpringBootApplication
 public class VersioningApplication {
+	private File file;
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(VersioningApplication.class, args);
---
+++ src/main/java/com/example/Versioning/controller/VersioningController.java
@@ -0,0 +1,34 @@
+package com.example.Versioning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+@RestController
+public class VersioningController {
+
+    @RequestMapping(value = "/")
+    public String home() throws Exception{ StringBuilder builder = new StringBuilder();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("C:\\Abdellah\\4IIR\\StageOritech\\testVersioning\\Versioning\\src\\main\\resources\\test.txt"));
+            String line = null;
+            while ((line = reader.readLine()) != null)
+                builder.append(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+        if (reader != null) {
+            try {
+                reader.close();
+            }
+            catch (IOException ignored) {}
+        }
+        }
+        return builder.toString();
+    }
+}
